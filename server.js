cd736a5c0b892017a58da10030009f34440e8c49
Mark Reynoso <mreynoso@spu.edu>
2017-10-02 14:57:06 -0700

    created and populated table, nodemon running

--- server.js
+++ server.js
@@ -4,7 +4,8 @@
 const EXPRESS = require('express');
 const PARSE = require('body-parser');
 const PG = require('pg');
-const PORT = process.env.PORT || 5000;
+const FS = require('fs');
+const PORT = process.env.PORT || 3000;
 const APP = EXPRESS();
 const CONSTRING = process.env.DATABASE_URL || 'postgres://localhost:5432/breeds';
 const REQUESTPROXY = require('express-request-proxy');
@@ -21,33 +22,35 @@ APP.get('/', function(request, response){
 function makeTable(request, response){
   CLIENT.query(`CREATE TABLE IF NOT EXISTS dogs (
       breed_id SERIAL PRIMARY KEY,
-      dogapi,
-      name,
-      size,
-      fur,
-      grooming,
-      activityLevel,
-      kids,
-      drools,
-      trainable,
-      needsYard,
-      allergies,
-      sheds);`)
-  .then(loadDogs)
-  .catch(console.error(err));
+      dogapi VARCHAR(255),
+      name VARCHAR(255),
+      size VARCHAR(255),
+      fur VARCHAR(255),
+      grooming INTEGER,
+      activityLevel INTEGER,
+      kids BOOLEAN,
+      drools BOOLEAN,
+      trainable INTEGER,
+      needsYard BOOLEAN,
+      allergies BOOLEAN,
+      sheds BOOLEAN)`)
+    .then(loadDogs)
+    .catch(console.error);
 }
 
 function loadDogs(){
-
-  CLIENT.query(`INSERT INTO dogs(dogapi, name, size, fur, grooming, activityLevel, kids, drools, trainable, needsYard, allergies, sheds) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) ON CONFLICT DO NOTHING`, [])
+  FS.readFile('./public/data/breeds.json', (err, fd) => {
+    JSON.parse(fd.toString()).forEach(ele => {
+      CLIENT.query(`INSERT INTO dogs(dogapi, name, size, fur, grooming, activityLevel, kids, drools, trainable, needsYard, allergies, sheds) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) ON CONFLICT DO NOTHING`, [ele.dogapi, ele.name, ele.size, ele.fur, ele.grooming, ele.activityLevel, ele.kids, ele.drools, ele.trainable, ele.needsYard, ele.allergies, ele.sheds])
+        .catch(function(err){
+          console.error(ele.name + ' broken\n' + err);
+        });
+    })
+  })
 }
-JSON.parse(fd.toString()).forEach(ele => {
-      client.query(
-        'INSERT INTO authors(author, "authorUrl") VALUES($1, $2) ON CONFLICT DO NOTHING',
-        [ele.author, ele.authorUrl]
-      )
+makeTable();
 
-CLIENT.query(`SELECT 'name' FROM 'breeds' WHERE ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`[request.allergy.])
+// CLIENT.query(`SELECT 'name' FROM 'breeds' WHERE ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`[request.allergy.])
 
 // function petRecommendation(request, response){
 //
