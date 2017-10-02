4c5bdfaefbd1e95e59fa601938400c6775d0e854
Mark Reynoso <mreynoso@spu.edu>
2017-10-02 14:15:35 -0700

    creating table and populating it in server.js

--- server.js
+++ server.js
@@ -18,9 +18,37 @@ APP.get('/', function(request, response){
 
 // APP.get('/match/', breedQuery);
 
-// function breedQuery(request, resonse){
-//   CLIENT.query(`SELECT 'breed' FROM 'breeds' WHERE ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`[request.allergy.])
-// }
+function makeTable(request, response){
+  CLIENT.query(`CREATE TABLE IF NOT EXISTS dogs (
+      breed_id SERIAL PRIMARY KEY,
+      dogapi,
+      name,
+      size,
+      fur,
+      grooming,
+      activityLevel,
+      kids,
+      drools,
+      trainable,
+      needsYard,
+      allergies,
+      sheds);`)
+  .then(loadDogs)
+  .catch(console.error(err));
+}
+
+function loadDogs(){
+
+  CLIENT.query(`INSERT INTO dogs(dogapi, name, size, fur, grooming, activityLevel, kids, drools, trainable, needsYard, allergies, sheds) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) ON CONFLICT DO NOTHING`, [])
+}
+JSON.parse(fd.toString()).forEach(ele => {
+      client.query(
+        'INSERT INTO authors(author, "authorUrl") VALUES($1, $2) ON CONFLICT DO NOTHING',
+        [ele.author, ele.authorUrl]
+      )
+
+CLIENT.query(`SELECT 'name' FROM 'breeds' WHERE ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`[request.allergy.])
+
 // function petRecommendation(request, response){
 //
 //   (requestProxy({
