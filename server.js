cc4dcb80778d03f2206830b3886c33a1f71b3d76
Mark Reynoso <mreynoso@spu.edu>
2017-10-02 12:26:16 -0700

    added get request for home route and began a call to the api

--- server.js
+++ server.js
@@ -11,4 +11,18 @@ const REQUESTPROXY = require('express-request-proxy');
 const CLIENT = new PG.Client(CONSTRING);
 CLIENT.connect();
 APP.use(EXPRESS.static('./public'));
+
+APP.get('/', function(request, response){
+  response.sendFile('index.html', {root: './public'});
+})
+
+APP.get('/match/', petRecommendation);
+
+function petRecommendation(request, response){
+  (requestProxy({
+    url: `http://api.petfinder.com/pet.find?key=f6a8dafe70de07dbc2e288483f909123${insert request params}`,
+    headers: {Authorization: `Key here or elsewhere?`}
+  }))()(request, response)
+}
+
 APP.listen(PORT, () => console.log(`Server running on port ${PORT}.`));
