81649b425dfd97a1ab0e179e2864cc66892df9dc
daniel <dpillay03@gmail.com>
2017-10-03 17:18:19 -0700

    made a new adoptable pet object and we accessed the api data

--- public/scripts/controller/form.js
+++ public/scripts/controller/form.js
@@ -17,7 +17,23 @@ var app = app || {};
       }
 
       function filterPets(response) {
-        console.log(response);
+        console.log(response.petfinder.pets.pet[0].media.photos.photo);
+
+        for (var i = 0; i < response.petfinder.pets.pet.length; i++) {
+          app.adoptablePets.push(new app.AdoptablePet(
+            response.petfinder.pets.pet[i].name.$t,
+            response.petfinder.pets.pet[i].breeds,
+            response.petfinder.pets.pet[i].contact, response.petfinder.pets.pet[i].media.photos.photo.filter(function(photo){return (photo['@size'] === 'x' && photo['@id'] === '1');})[0].$t, response.petfinder.pets.pet[i].sex.$t
+          ));
+
+
+          // app.adoptablePets.name = response.petfinder.pets.pet[i].name;
+          // app.adoptablePets.breeds = response.petfinder.pets.pet[i].breeds;
+          // app.adoptablePets.contact = response.petfinder.pets.pet[i].contact;
+          // app.adoptablePets.sex = response.petfinder.pets.pet[i].sex;
+          // app.adoptablePets.photo = response.petfinder.pets.pet[i].media.photos.photo.filter(function(photo){return (photo['@size'] === 'x' && photo['@id'] === '1');})[0].$t;
+
+        }
       }
 
       let data = {
