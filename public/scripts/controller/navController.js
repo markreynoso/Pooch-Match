750d7803b7b799f05547d3a488b9577c24fc4e34
Kinley Ramson <kinleyramson@gmail.com>
2017-10-05 16:11:33 -0700

    more styles

--- public/scripts/controller/navController.js
+++ public/scripts/controller/navController.js
@@ -1,17 +1,6 @@
 'use strict';
 
-$('#menu-icon').on('click', function(){
-  $('#navlist').toggle();
-});
-
-// $('#navlist').on('click', function(){
-//   $('#navlist').toggle();
-// });
 
 $(window).on('resize', function(){
   window.screen.width > 650 ? $('#navlist').show() : $('#navlist').hide();
 })
-
-// $('#noBreeds').on('click', function(){
-//   app.quizController.index();
-// })
